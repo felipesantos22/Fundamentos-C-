91ece66f600e2a271717d5168111ba94b627c31f
felipesantos22 <felipevs2018@gmail.com>
2024-01-01 22:32:12 -0300

    first commit

---
+++ cursocsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using cursocsharp.fundamentos;
+
+Console.WriteLine("Hello, World!");
+var variable = new Variable();
+Variable.Test();
+variable.Age();
+var nome = variable.Name("Felipe Virissimo Dos Santos");
+Console.WriteLine(nome);
+const int age = int.MaxValue;
+Console.WriteLine(age);
+
+Console.WriteLine("Qual seu nome?");
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Feliz 2024 {name}");
+
+const double value = 15.157;
+Console.WriteLine(value.ToString("F2"));
+Console.WriteLine(value.ToString("C"));
+Console.WriteLine(value.ToString("P"));
---
+++ cursocsharp/fundamentos/Variable.cs
@@ -0,0 +1,21 @@
+﻿namespace cursocsharp.fundamentos;
+
+public class Variable
+{
+    private int age = 10;
+    private string _name = "Felipe Santos";
+    public static void Test()
+    {
+        Console.WriteLine("Felipe");
+    }
+
+    public void Age()
+    {
+        Console.WriteLine(age);
+    }
+
+    public string Name(string name1)
+    {
+        return this._name = name1;
+    }
+}
